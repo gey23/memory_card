d3b2c87920abafaf3d02c27d03abff6ad148f655
gey23 <145996317+gey23@users.noreply.github.com>
2023-09-25 16:29:34 +0300

    Add files via upload

---
+++ my_memory_card.py
@@ -0,0 +1,139 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication,QButtonGroup, QWidget,QHBoxLayout, QVBoxLayout,QGroupBox, QRadioButton,QPushButton, QLabel)
+from random import randint, shuffle
+
+class   Question():
+    '''содержит вопрос, правельный ответ и три неправельных'''
+    def  __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+questions_list = []
+questions_list.append(Question('Сколько цветов на флаге России?','3','5','2','6'))
+questions_list.append(Question('Что едят коровы?','траву','шоколад','мороженое','шаурму'))
+questions_list.append(Question('Сколько мне лет?','15','13','20','16'))
+questions_list.append(Question('Сколько букв в слове мороженое','9','5','10','12'))
+questions_list.append(Question('Как зовут Сашу','Саша','Коля','Витя','Миша'))
+questions_list.append(Question('Кккого цвета небо','','','',''))
+'''questions_list.append(Question('','','','',''))
+questions_list.append(Question('','','','',''))
+questions_list.append(Question('','','','',''))
+questions_list.append(Question('','','','',''))
+questions_list.append(Question('','','','',''))'''
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Memory Card')
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Какой национальности не существует?')
+RadioGroupBox = QGroupBox("Варианты ответов")
+rbtn_1 = QRadioButton('Энцы')
+rbtn_2 = QRadioButton('Смурфы')
+rbtn_3 = QRadioButton('Чулымцы')
+rbtn_4 = QRadioButton('Алеуты')
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+RadioGroupBox.setLayout(layout_ans1)
+layout_line1 = QHBoxLayout() 
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+
+layout_card = QVBoxLayout()
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_line1.addWidget(lb_Question)
+layout_line2.addWidget(RadioGroupBox)   
+layout_line3.addWidget(btn_OK, stretch=2)
+
+AnsGroupBox = QGroupBox("Резуьтат теста")
+lb_Result = QLabel('прав ты или нет?') 
+lb_Correct = QLabel('ответ будет тут!')
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result)
+layout_res.addWidget(lb_Correct)
+AnsGroupBox.setLayout(layout_res)
+
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True)
+def test():
+    if 'Ответить' == btn_OK.text():
+        show_result()
+    else:
+        show_question()
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+def ask (q: Question):
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question)
+    lb_Correct.setText(q.right_answer)
+    show_question()
+def show_correst(res):
+    lb_Result.setText(res)
+    show_result()
+def check_ansver():
+    if answers[0].isChecked():
+        show_correst('Правельно!')
+        window.total += 1
+        print('Статистика\n-Всего вопросов:', window.total, '\n-Правельных ответов:', window.score)
+        print('Рейтинг: ', (window.score/window.total*100), '%')
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            show_correst('Неверно!')
+    
+def next_question():
+    '''Задаёт следующий вопрос из списка'''
+    window.total += 1
+    print('Статистика\n-Всего вопросов:', window.total, '\n-Правельных ответов: ', window.score)
+    cur_question = randint(0, len(questions_list) - 1)
+    q = questions_list[cur_question]
+    ask(q)
+def click_OK():
+    '''определяет, надо ли показывать другой вопрос либо потвердить ответ на этот'''
+    if btn_OK.text() == 'Ответить':
+        check_ansver()
+    else:
+        next_question()
+
+
+    
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+window.cur_question = -1
+btn_OK.clicked.connect(click_OK) 
+window.score = 0
+window.total = 0 
+next_question()
+window.resize(400, 300)
+window.show()
+app.exec()
